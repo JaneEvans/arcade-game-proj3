be3a8245c9e96ad90036037a3810fb94f37cbcab
JaneEvans <jinjin.ge@utexas.edu>
2018-09-16 18:06:54 -0700

    Initialize background canvas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // Enemies our player must avoid
-const Enemy = function() {
+// const Enemy = function() {
+function Enemy(){
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = '../images/enemy-bug.png';
+    this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
@@ -22,15 +23,30 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+function Player(){
 
+    this.sprite = 'images/char-cat-girl.png';
+}
+
+// This class requires an update(), render() and
+Player.prototype.update = function(dt){
+
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// a handleInput() method.
+Player.prototype.handleInput = function(allowedKey){
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [new Enemy()];
-// Place the player object in a variable called player
 
+// Place the player object in a variable called player
+let player = new Player();
 
 
 // This listens for key presses and sends the keys to your
